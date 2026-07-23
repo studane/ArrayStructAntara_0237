e6033bafd15f707345011097d001f3721ee66fad
studane <zhidaneprabowoguzlend@gmail.com>
2026-07-23 14:26:28 +0700

    menampilkan data

--- struct.cpp
+++ struct.cpp
@@ -21,5 +21,8 @@ int main()
     cin >> mhs.umur;
     cout << endl;
 
-    
+    cout << "Data Tampil" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
 }
