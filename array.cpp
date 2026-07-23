0254c34fc0d5f6081335094ae366f0dbc6754fa5
studane <zhidaneprabowoguzlend@gmail.com>
2026-07-23 14:11:24 +0700

    menampilkan data dengan perintah satu persatu

--- array.cpp
+++ array.cpp
@@ -26,4 +26,15 @@ int main()
     cout << "Isikan data kelima : ";
     cin >> datamu [4];
     cout << endl;
+
+
+    //Menampilkan data dengan dengan perintah satu persatu
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
+    cout << endl;
+
+    
 }
